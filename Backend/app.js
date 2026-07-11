635ccc09ef21da87bc9b64c1669700a04066796b
Harsh Gosalia <harshgosalia05l@gmail.com>
2026-07-11 13:35:41 +0530

    First Commit

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+let port = 9000;
+
+app.get('/ping', (req,res) => {
+    res.send({
+        message: 'Hello, World!'
+    })
+});
+
+
+app.get('/',(req,res) => {
+    res.send({
+        message: 'Welcome to the Backend API!'
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
